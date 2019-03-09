59aea05ae7bc0765185f495685a580e99f588b53
zhuzhe <zhuzhe@findpiano.cn>
2019-03-09 09:49:05 +0800

    添加按需加载示例

--- h5/babel.config.js
+++ h5/babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    ['@nutui/babel-plugin-separate-import', {
+      'style': 'css'
+    }],
+    [
+      'import', {
+        libraryName: 'vant',
+        libraryDirectory: 'es',
+        style: true
+      }
+    ]
   ]
 }
--- h5/src/tab/my/App.vue
+++ h5/src/tab/my/App.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
       my
+      <!-- <van-button type="default">默认按钮</van-button> -->
+      <nut-button small>去结算</nut-button>
     </div>
 </template>
 <style scoped>
 
 </style>
 <script>
+// import { Button } from 'vant'
+import { Button } from '@nutui/nutui'
 export default {
   data () {
     return {
     }
+  },
+  components: {
+    // [Button.name]: Button,
+    [Button.name]: Button
   },
   mounted () {
     console.log('mounted', this)
